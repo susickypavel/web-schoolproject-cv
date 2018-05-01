43524b1861a25ba1c5188dbae4e24ecce67b6b82
Thesoreon <pavels.sus@gmail.com>
2018-05-01 23:48:58 +0200

    - Added meta tags

--- src/components/homepage.component.tsx
+++ src/components/homepage.component.tsx
@@ -7,15 +7,20 @@ interface IProps {
 export  default class extends React.Component<IProps> {
     render() {
         return(
-            <div className="row">
-                <div className="col-sm red">
-                    TeST
+            <div>
+                <div className="jumbotron">
+                    Haha
                 </div>
-                <div className="col-sm lime">
-                    TeST
-                </div>
-                <div className="col-sm blue">
-                    BLUE
+                <div className="row">
+                    <div className="col-sm red">
+                        TeST
+                    </div>
+                    <div className="col-sm lime">
+                        TeST
+                    </div>
+                    <div className="col-sm blue">
+                        BLUE
+                    </div>
                 </div>
             </div>
         );
