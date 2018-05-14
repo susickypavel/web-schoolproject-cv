465bd89fc0537dbb3fe9ca04f60261aa2e652607
Thesoreon <pavels.sus@gmail.com>
2018-05-14 22:41:19 +0200

    - Added spinner after submitting the contact form
    - Connected contact form to backend side

--- src/components/contactpage.component.tsx
+++ src/components/contactpage.component.tsx
@@ -1,17 +1,54 @@
 import * as React from "react";
-import { connect } from "react-redux";
+import axios from "axios";
+import { History } from "history";
 
 import ContactForm from "../containers/contactform.container";
 
-export default class ContactPage extends React.Component {
+interface State {
+    pending: boolean;
+}
+
+interface Props {
+    history: History;
+}
+
+export default class ContactPage extends React.Component<Props, State> {
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            pending: false
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
     handleSubmit(values: any) {
-        console.log(values);
+        const promise = axios.post("https://web-portfolio-email.herokuapp.com/", values).then(() => {
+            this.setState({ pending: false });
+
+            this.props.history.push("/");
+        });
+
+        this.setState({ pending: true });
+    }
+
+    renderSpinner() {
+        return(
+            <div className="spinner">
+                <div className="spinner-holder">
+                    <div className="spinner-circle" />
+                    <p>This might take a few seconds</p>
+                </div>
+            </div>
+        );
     }
 
     render() {
         return(
             <div>
+                {this.state.pending ? this.renderSpinner() : null}
                 <img src={require("../assets/images/logo.svg")} alt="logo" className="img-fluid mx-auto d-block mt-3"/>
                 <ContactForm onSubmit={this.handleSubmit}/>
             </div>
--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import * as promise from "redux-promise";
 
 import reducers from "./reducers";
 
 export function initializeStore() {
     const enhancers = [
-        applyMiddleware()
+        applyMiddleware(promise)
     ];
 
     const createStoreWithMiddleware = composeWithDevTools(...enhancers)(createStore);
