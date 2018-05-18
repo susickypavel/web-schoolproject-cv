2dadb92add203c64daa940905bb95fba069b8f2a
Thesoreon <pavels.sus@gmail.com>
2018-05-18 23:38:18 +0200

    - Added new property to objects on ticketlist.reducer (badge)
    - Added react transition to projectspage.component content
    - Added functionality, only projects with corresponding badge are displayed.

--- src/components/aboutpage.component.tsx
+++ src/components/aboutpage.component.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 
+
+// Make tickets with progress bar of each framework etc. i'm using right now
+// Animate the progress bar? Filling?
+
 export default class AboutPage extends React.Component {
     render() {
         return(
